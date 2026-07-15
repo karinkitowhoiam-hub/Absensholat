c7e2b5ac940a8ad561fcf0682f13de137ea1630a
karinkitowhoiam-hub <karinkitowhoiam@gmail.com>
2026-07-15 19:32:37 +0800

    Create koneksi.php

---
+++ absen_sholat_v2/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+// PERBAIKI BAGIAN INI:
+$db   = "db_absen_sholat_v2"; // <--- Pastikan ada _v2 nya!
+
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+// ....
+?>
